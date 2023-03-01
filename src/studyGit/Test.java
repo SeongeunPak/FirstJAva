dad0cbc29218e51a1767d491f69b83abb9f2d6f0
user2 <airisan0419@naver.com>
2023-03-01 22:51:26 +0900

    modified

--- src/studyGit/Test.java
+++ src/studyGit/Test.java
@@ -7,6 +7,7 @@ public class Test
 {		int n = 0;
 
 		System.out.println("test");
+		System.out.println("hello");
 		System.out.println(n);
 		System.out.println("--수정--");
 	        System.out.println("--수정2--");
